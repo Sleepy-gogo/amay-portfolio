60c32913cb3b72750fe3cebe881cd2f946443f31
Axel Cast <cast.axel27@gmail.com>
2023-07-07 05:28:43 -0300

    Added the carousel to the project

--- js/script.js
+++ js/script.js
@@ -13,3 +13,45 @@ navToggle.addEventListener('click', () => {
 	navigation.setAttribute('data-visible', visibility);
 	navToggle.setAttribute('aria-expanded', visibility);
 });
+
+let indexActivo = 0;
+
+const proyectos = document.getElementsByClassName("proyecto");
+
+function obtenerSiguienteIndice(n) {
+	return n + 1 <= proyectos.length - 1 ? n + 1 : 0;
+}
+
+function obtenerPrevioIndice(n) {
+	return n - 1 >= 0 ? n -1 : proyectos.length - 1;
+}
+
+const handleNext = () => {
+	const nextIndex = obtenerSiguienteIndice(indexActivo);
+	const nextOfNextIndex = obtenerSiguienteIndice(nextIndex);
+
+	const proyectoActual = document.querySelector(`[data-index="${indexActivo}"]`),
+		  nextProyecto = document.querySelector(`[data-index="${nextIndex}"]`),
+		  futuroProyecto = document.querySelector(`[data-index="${nextOfNextIndex}"]`)
+
+	proyectoActual.setAttribute('data-state', "before");
+	nextProyecto.setAttribute('data-state', "active");
+	futuroProyecto.setAttribute('data-state', "after");
+
+	indexActivo = nextIndex;
+};
+
+const handleBack = () => {
+	const backIndex = obtenerPrevioIndice(indexActivo);
+	const backOfBackIndex = obtenerPrevioIndice(backIndex);
+
+	const proyectoActual = document.querySelector(`[data-index="${indexActivo}"]`),
+		  backProyecto = document.querySelector(`[data-index="${backIndex}"]`),
+		  previoAbackProyecto = document.querySelector(`[data-index="${backOfBackIndex}"]`)
+
+	proyectoActual.setAttribute('data-state', "after");
+	backProyecto.setAttribute('data-state', "active");
+	previoAbackProyecto.setAttribute('data-state', "before");
+
+	indexActivo = backIndex;
+};
