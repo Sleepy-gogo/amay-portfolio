0695d8081e1fee0a33abdce890039720754c5d72
Axel Cast <cast.axel27@gmail.com>
2023-07-06 21:59:19 -0300

    Added responsive navbar

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ window.addEventListener("scroll", () => {
 	let header = document.querySelector("header");
 	header.classList.toggle("header-opaco", window.scrollY > 0);
 });
+
+const navigation = document.querySelector('.navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+	const visible = navigation.getAttribute('data-visible') == "true";
+	const visibility = !visible
+
+	navigation.setAttribute('data-visible', visibility);
+	navToggle.setAttribute('aria-expanded', visibility);
+});
